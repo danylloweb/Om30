b3ac5cad70a1f9dfa704b0886b0d4edaf1b00e2c
danyllo <danyllophp@gmail.com>
2023-11-06 11:32:31 -0300

    adicionando Validator

--- Om30/app/Validators/PatientValidator.php
+++ Om30/app/Validators/PatientValidator.php
@@ -18,7 +18,22 @@ class PatientValidator extends LaravelValidator
      * @var array
      */
     protected $rules = [
-        ValidatorInterface::RULE_CREATE => [],
+        ValidatorInterface::RULE_CREATE => [
+            'full_name'     => 'required|string',
+            'full_name_mom' => 'required|string',
+            'cpf'           => 'max:11',
+            'cns'           => 'required|string|unique:patients'
+        ],
         ValidatorInterface::RULE_UPDATE => [],
+    ];
+    protected $messages = [
+        'full_name.required'      => 'Campo Nome Obrigatório',
+        'full_name.string'        => 'Formato Incorreto do nome',
+        'full_name_mom.required'  => 'Campo Nome da mãe Obrigatório',
+        'full_name_mom.string'    => 'Formato Incorreto do nome da mãe',
+        'cpf.max'                 => 'Tamanho excedido do cpf',
+        'cns.required'            => 'Cartão nacional de saude obrgatório',
+        'cns.string'              => 'Formato Incorreto do cns',
+        'cns.unique'              => 'Cartão nacional de saude já cadastrado',
     ];
 }
